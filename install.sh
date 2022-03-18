91460b1633e1886e88705300d4cbcbb202531a25
Luis Gonzalo Patiño <96958439+luisg-pat@users.noreply.github.com>
2022-03-18 15:59:40 -0500

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Install drivers, i3, xorg, picom, alacritty and sddm
+sudo pacman -Syu
 sudo pacman -S xf86-video-nouveau xf86-video-intel mesa i3-gaps i3blocks xorg picom alacritty sddm vim xorg-setxkbmap rofi pulseaudio man wget pavucontrol nitrogen lxappearance arch-theme 
 
 # Installing yay
 sudo pacman -S --need base-devel
-sudo pacman -S git
 sudo git clone https://aur.archlinux.org/yay.git
 sudo chown -R luis:luis yay
 cd yay
@@ -23,4 +23,8 @@ unzip fontawesome-free-6.1.0-web.zip
 mkdir ~/.fonts
 mv ./.fontawesome-fre-6.1.0-web.zip/webfonts/*.ttf ~/.fonts
 
-
+# Copying i3 config
+cp ./config ~/.config/i3/config
+cp -r ./scripts ~/.config/i3/
+# copying libinput conf
+sudo cp ./post_inst/40-libinput.conf /usr/share/X11/xorg.conf.d/
